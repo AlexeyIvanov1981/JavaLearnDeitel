6c61ca47eede442f9f3742096ef77882566b892d
AlexeyIvanov1981 <lelikus81@yandex.ru>
2023-03-03 12:26:35 +0300

    added ex33

---
+++ src/ch4/ex33/Ex33.java
@@ -0,0 +1,13 @@
+package ch4.ex33;
+
+public class Ex33 {
+    public static void main(String[] args) {
+
+        int x = 2;
+
+        while (true) {
+            System.out.println(x);
+            x += 2;
+        }
+    }
+}
