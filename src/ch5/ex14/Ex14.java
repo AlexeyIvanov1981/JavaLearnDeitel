fd04a08d2d7b13ffac9296b0634cdb85066e3543
AlexeyIvanov1981 <lelikus81@yandex.ru>
2023-03-16 11:22:40 +0300

    add ex14

--- src/ch5/ex14/Ex14.java
+++ src/ch5/ex14/Ex14.java
@@ -1,4 +1,20 @@
 package ch5.ex14;
 
 public class Ex14 {
+    public static void main(String[] args) {
+        double amount;
+        double principal = 1000.0;
+        double rate = 0.05;
+
+        for (int i = 5; i <= 10; i++){
+            System.out.printf("%s%20s%10s%n", "Year", "Amount on deposit", "Rate");
+
+            for (int year = 1; year <= 10; ++year){
+                amount = principal * Math.pow(1.0 + rate, year);
+                System.out.printf("%4d%,20.2f%,10.2f%n", year, amount, rate);
+            }
+            System.out.println();
+            rate = rate + 0.01;
+        }
+    }
 }
