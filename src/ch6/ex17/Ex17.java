3a33f12c436aec7c8ec7df8b31d6f84e6ee088ca
AlexeyIvanov1981 <lelikus81@yandex.ru>
2023-03-25 20:18:47 +0300

    Added Ex17.java

--- src/ch6/ex17/Ex17.java
+++ src/ch6/ex17/Ex17.java
@@ -17,12 +17,6 @@ public class Ex17 {
     }
 
     public static boolean isEven(int number) {
-        boolean result;
-        if (number % 2 == 0) {
-            result = true;
-        } else {
-            result = false;
-        }
-        return result;
+        return number % 2 == 0;
     }
 }
