b93bfdeb3c19ddc09446ac8a9cc1945f0c625882
AlexeyIvanov1981 <lelikus81@yandex.ru>
2023-03-25 20:16:09 +0300

    Added Ex17.java

--- src/ch6/ex17/Ex17.java
+++ src/ch6/ex17/Ex17.java
@@ -1,6 +1,5 @@
 package ch6.ex17;
 
-import javax.lang.model.util.SimpleAnnotationValueVisitor6;
 import java.util.Scanner;
 
 public class Ex17 {
@@ -10,16 +9,16 @@ public class Ex17 {
 
         int number = scanner.nextInt();
 
-        if (number <=0) {
+        if (number <= 0) {
             System.out.print("Error");
         } else {
             System.out.print("Number is even - " + isEven(number));
         }
     }
 
-    public static boolean isEven (int number){
+    public static boolean isEven(int number) {
         boolean result;
-        if (number % 2 == 0){
+        if (number % 2 == 0) {
             result = true;
         } else {
             result = false;
