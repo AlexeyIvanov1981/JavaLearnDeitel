32fe420d4b4fd3683ea3b1fc936f7fe4ec498e4e
AlexeyIvanov1981 <lelikus81@yandex.ru>
2023-03-25 20:04:30 +0300

    add Ex14&16

--- src/ch6/ex14/Ex14.java
+++ src/ch6/ex14/Ex14.java
@@ -1,2 +1,26 @@
-package ch6.ex14;public class Ex14 {
+package ch6.ex14;
+
+import java.util.Scanner;
+
+public class Ex14 {
+    public static int exponentiation(int base, int exponent) {
+        int result = base;
+
+        for (int i = 1; i < exponent; i++) {
+            result *= base;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter base number: ");
+        int base = scanner.nextInt();
+
+        System.out.print("Enter exponent number: ");
+        int exponent = scanner.nextInt();
+
+        System.out.print("Result = " + exponentiation(base, exponent));
+    }
 }
--- src/ch6/ex16/Ex16.java
+++ src/ch6/ex16/Ex16.java
@@ -1,2 +1,23 @@
-package ch6.ex16;public class Ex16 {
+package ch6.ex16;
+
+import java.util.Scanner;
+
+public class Ex16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter two values separated by spaces: ");
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        System.out.print("second value in each pair is a multiple of the first - " + isMultiple(number1, number2));
+    }
+
+    public static boolean isMultiple (int number1, int number2) {
+        boolean result;
+        if (number2 % number1 == 0) {
+            result = true;
+        } else {
+            result = false;
+        }
+        return result;
+    }
 }
