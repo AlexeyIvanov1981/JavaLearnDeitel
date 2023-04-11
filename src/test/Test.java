ddd6e8a0a0355109a55cf1bc73fe03ca1a707cde
AlexeyIvanov1981 <lelikus81@yandex.ru>
2023-04-11 19:56:31 +0300

    add classes Time2.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,7 +1,11 @@
 package test;
 
+
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println(13 / 13);
+        String[] arr = new String[5];
+        System.out.println(Arrays.toString(arr));
     }
 }
