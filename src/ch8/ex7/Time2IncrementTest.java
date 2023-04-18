e5dfb9a44ad918fd7640dc86b78f1fa6b870fce2
AlexeyIvanov1981 <lelikus81@yandex.ru>
2023-04-18 11:43:10 +0300

    some fix in Tine2IncrementTest.java

--- src/ch8/ex7/Time2IncrementTest.java
+++ src/ch8/ex7/Time2IncrementTest.java
@@ -2,11 +2,14 @@ package ch8.ex7;
 
 public class Time2IncrementTest {
     public static void main(String[] args) {
-        Time2Increment time = new Time2Increment(11, 58, 59);
+        Time2Increment time = new Time2Increment(11, 59, 59);
+
         System.out.printf("time before increment: %s%n", time.toUniversalString());
-        System.out.println();
+        System.out.printf("12/24: %s%n", time.toString());
 
         time.tick();
+
         System.out.printf("Time after increment: %s%n", time.toUniversalString());
+        System.out.printf("12/24: %s%n", time.toString());
     }
 }
