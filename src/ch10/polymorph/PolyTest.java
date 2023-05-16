67716769e893845cb4399d38c65d7270e32d3a31
Alexey.Ivanov1981 <lelikus81@yandex.ru>
2023-05-16 17:32:55 +0300

    trable with pattern

--- src/ch10/polymorph/PolyTest.java
+++ src/ch10/polymorph/PolyTest.java
@@ -2,10 +2,14 @@ package ch10.polymorph;
 
 public class PolyTest {
     public static void main(String[] args) {
-        SalariedEmployee salariedEmployee = new SalariedEmployee("John", "Smith", "111-11-1111", 800.00);
-        HourlyEmployee hourlyEmployee = new HourlyEmployee("Karen", "Price", "222-22-2222", 16.75, 40);
-        CommissionEmployee commissionEmployee = new CommissionEmployee("Sue", "Jones", "333-33-3333", 10000.00, 0.06);
-        BasePlusCommissionEmployee basePlusCommissionEmployee = new BasePlusCommissionEmployee("Bob", "Lewis", "444-44-4444", 5000, 0.04, 300);
+        SalariedEmployee salariedEmployee = new SalariedEmployee("John", "Smith",
+                "111-11-1111", 800.00);
+        HourlyEmployee hourlyEmployee = new HourlyEmployee("Karen", "Price",
+                "222-22-2222", 16.75, 40);
+        CommissionEmployee commissionEmployee = new CommissionEmployee("Sue", "Jones",
+                "333-33-3333", 10000.00, 0.06);
+        BasePlusCommissionEmployee basePlusCommissionEmployee = new BasePlusCommissionEmployee("Bob",
+                "Lewis", "444-44-4444", 5000, 0.04, 300);
 
         System.out.println("Employees processed individually:");
 
@@ -30,12 +34,17 @@ public class PolyTest {
         for (Employee currentEmployee : employees) {
             System.out.println(currentEmployee);
 
-//            if currentEmployee instanceof BasePlusCommissionEmployee() {
-//
-//                BasePlusCommissionEmployee employee = (BasePlusCommissionEmployee) currentEmployee;
-//                employee.setBaseSalary(1.10 * employee.getBaseSalary());
-//                System.out.printf("new base salary with 10%% increase is: $%,.2f%n", employee.getBaseSalary());
-//            }
+            if (currentEmployee instanceof BasePlusCommissionEmployee) {
+
+                BasePlusCommissionEmployee employee = (BasePlusCommissionEmployee) currentEmployee;
+                employee.setBaseSalary(1.10 * employee.getBaseSalary());
+                System.out.printf("new base salary with 10%% increase is: $%,.2f%n", employee.getBaseSalary());
+            }
+            System.out.printf("earned $%,.2f%n%n", currentEmployee.earnings());
+        }
+        for (int j = 0; j < employees.length; j++) {
+            System.out.printf("Employee %d is a %s%n", j, employees[j].getClass().getName());
         }
     }
 }
+
