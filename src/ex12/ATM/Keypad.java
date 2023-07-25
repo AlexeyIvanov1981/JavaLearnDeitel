611b8ed1602b1d55344d8d4f848500c29c7b5943
AlexeyIvanov1981 <lelikus81@yandex.ru>
2023-07-25 12:44:11 +0300

    finished ex making from ch10

---
+++ src/ex12/ATM/Keypad.java
@@ -0,0 +1,11 @@
+package ex12.ATM;
+
+public class Keypad {
+    public Keypad(){
+
+    }
+
+    public void getInput(){
+
+    }
+}
---
+++ src/ex12/ATM/Withdrawal.java
@@ -0,0 +1,23 @@
+package ex12.ATM;
+
+import java.security.PrivateKey;
+
+public class Withdrawal {
+
+    private int accountNumber;
+    private double amount;
+    private Screen screen;
+    private Keypad keypad;
+    private CashDispenser cashDispenser;
+    private BankDatabase bankDatabase;
+
+    public Withdrawal() {
+
+    }
+
+    public void execute(){
+
+    }
+
+
+}
