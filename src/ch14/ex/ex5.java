371b852e2a5457249cc333b404239e436e7c839a
Alexey.Ivanov1981 <lelikus81@yandex.ru>
2023-09-14 09:55:10 +0300

    added some fix in ex5.java

--- src/ch14/ex/ex5.java
+++ src/ch14/ex/ex5.java
@@ -2,6 +2,8 @@ package ch14.ex;
 
 import java.util.Random;
 
+import static ch14.ex.RandomNumber.randomNumber;
+
 public class ex5 {
     public static void main(String[] args) {
         String[] article = {"the", "a", "one", "some", "anny"};
@@ -14,23 +16,14 @@ public class ex5 {
             String nounWord = noun[randomNumber(article.length)];
             String verbWord = verb[randomNumber(article.length)];
             String prepositionWord = preposition[randomNumber(article.length)];
-            StringBuilder sb = new StringBuilder();
-            String result = sb.
-                    append(articleWord).
-                    append(" ").
-                    append(nounWord).
-                    append(" ").
-                    append(verbWord).
-                    append(" ").
-                    append(prepositionWord).
-                    append(".").toString();
+
+            String result = articleWord + " " + nounWord + " " + verbWord + " " + prepositionWord + ".";
 
             System.out.println(result.substring(0, 1).toUpperCase() + result.substring(1));
         }
-
-
     }
-
+}
+class RandomNumber{
     public static int randomNumber(int endNumber) {
         Random random = new Random();
         return random.nextInt(endNumber);
