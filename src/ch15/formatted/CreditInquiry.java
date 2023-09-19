42b198613b67263fbcc10483525b2788b5172ef0
AlexeyIvanov1981 <lelikus81@yandex.ru>
2023-09-19 21:34:13 +0300

    start ex15

--- src/ch15/formatted/CreditInquiry.java
+++ src/ch15/formatted/CreditInquiry.java
@@ -1,5 +1,7 @@
 package ch15.formatted;
 
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
@@ -8,6 +10,23 @@ public class CreditInquiry {
 
     public static void main(String[] args) {
         MenuOption accountType = getRequest();
+
+        while (accountType != MenuOption.END){
+            switch (accountType){
+                case ZERO_BALANCE:
+                    System.out.printf("%nAccounts with zero balances:%n");
+                    break;
+                case CREDIT_BALANCE:
+                    System.out.printf("%nAccounts with credit balances:%n");
+                    break;
+                case DEBIT_BALANCE:
+                    System.out.printf("%nAccounts with debit balances:%n");
+                    break;
+            }
+
+            readRecords(accountType);
+            accountType = getRequest();
+        }
     }
 
     private static MenuOption getRequest() {
@@ -24,9 +43,35 @@ public class CreditInquiry {
                 System.out.printf("%n? ");
                 request = input.nextInt();
             } while ((request < 1) || (request > 4));
-        } catch (NoSuchElementException elementException){
+        } catch (NoSuchElementException elementException) {
             System.err.println("Invalid input. Terminating.");
         }
         return choices[request - 1];
+    }
+
+    private static void readRecords(MenuOption accountType) {
+        try (Scanner input = new Scanner(Paths.get("clients.txt"))) {
+            while (input.hasNext()) {
+                int accountNumber = input.nextInt();
+                String firstName = input.next();
+                String lastName = input.next();
+                double balance = input.nextDouble();
+                if (shouldDisplay(accountType, balance))
+                    System.out.printf("%-10d%-12s%-12s%10.2f%n", accountNumber,
+                            firstName, lastName, balance);
+                else input.nextLine();
+            }
+        } catch (NoSuchElementException | IllegalStateException | IOException e) {
+            System.err.println("Error processing file. Terminating.");
+            System.exit(1);
+        }
+    }
+
+    private static boolean shouldDisplay(MenuOption accountType, double balance) {
+        if ((accountType == MenuOption.CREDIT_BALANCE) && (balance < 0)) return true;
+        else if ((accountType == MenuOption.DEBIT_BALANCE) && (balance > 0)) return true;
+        else if ((accountType == MenuOption.ZERO_BALANCE) && (balance == 0)) return true;
+
+        return false;
     }
 }
